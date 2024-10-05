771b418a222dbba64fbb98bd183429afd8b52412
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-05 18:04:35 +0300

    threejs setup

--- components/home/gallery/index.tsx
+++ components/home/gallery/index.tsx
@@ -1,10 +1,15 @@
 import RenderModel from "@/components/three/render";
 import Venus from "@/components/three/models/planet";
+import Img from "@/public/home/herobg.jpg";
 
 const GallerySection = () => {
   return (
-    <section className="border p-2 flex-1">
-      <h1>Gallery Section</h1>
+    <section
+      className="border p-2 flex-1 bg-cover bg-center relative h-screen "
+      style={{ backgroundImage: `url(${Img.src})` }}
+    >
+      <h1 className="relative z-10 text-white">Gallery Section</h1>
+
       {/* Render the 3D Sphere */}
       <RenderModel>
         <Venus position={[0, 0, 0]} />
