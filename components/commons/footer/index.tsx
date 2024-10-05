440a7e154b4936dc070026fb036456db52602b1b
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-05 21:36:22 +0300

    first setup

--- components/commons/footer/index.tsx
+++
@@ -1,9 +0,0 @@
-const FooterMain = () => {
-  return (
-    <footer className="bg-green-800 text-white mt-auto">
-      <div>Footer</div>
-    </footer>
-  );
-};
-
-export default FooterMain;
--- components/home/banner/index.tsx
+++
@@ -1,9 +0,0 @@
-const BannerSection = () => {
-  return (
-    <section className="border p-2 flex-1">
-      <h1>Banner Section</h1>
-    </section>
-  );
-};
-
-export default BannerSection;
