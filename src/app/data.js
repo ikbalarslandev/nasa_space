3a5afbd0dafd0000d330f404c18c29adee2282bf
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-06 05:30:33 +0300

    home setup with icons

--- src/app/data.js
+++ src/app/data.js
@@ -91,7 +91,7 @@ export const projectsData = [
 
 export const BtnList = [
   { label: "Home", link: "/", icon: "home", newTab: false },
-  { label: "About", link: "/about", icon: "about", newTab: false },
-  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
-  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  { label: "Factory", link: "/about", icon: "about", newTab: false },
+  { label: "Life", link: "/projects", icon: "projects", newTab: false },
+  { label: "Fun", link: "/contact", icon: "contact", newTab: false },
 ];
